be711aced78efaa4e3d3080c421f4d3eb9b10ac0
ywoo121 <ywoo121@naver.com>
2022-05-26 03:41:28 +0900

    Logo&Icons

---
+++ owl-mailbox-front/src/Icons/Resources.js
@@ -0,0 +1,29 @@
+import bookmark from "../Icons/bookmark.png";
+import siren from "../Icons/siren.png";
+import postage_stamp from "../Icons/postage_stamp.png";
+import left_arrow from "../Icons/left_arrow.png";
+import right_arrow from "../Icons/right_arrow.png"
+import main_logo from "../Icons/main_logo.png";
+import kakao_login_btn from "../Icons/kakao_login_btn.png";
+
+export const boardIcons = {
+    bookmark,
+    siren,
+};
+
+export const stamps = {
+    postage_stamp,
+}
+
+export const arrows = {
+    left_arrow,
+    right_arrow,
+}
+
+export const logos = {
+    main_logo,
+}
+
+export const login = {
+    kakao_login_btn,
+}
--- owl-mailbox-front/src/index.js
+++ owl-mailbox-front/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Board from "./Board";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Board />
   </React.StrictMode>
 );
+
+//TODO : Router 사용해서 루트짜기
+//TODO : NavBot.. 돌려막기할 수 있게 새로 js 작성. 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
