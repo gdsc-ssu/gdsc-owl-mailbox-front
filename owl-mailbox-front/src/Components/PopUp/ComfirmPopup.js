28198265a5a3367e3ad26da635933b4298b3ca9b
yoo-jimin127 <dbwlals9936@naver.com>
2022-05-29 01:41:57 +0900

    fix: 팝업 file name 변경

--- owl-mailbox-front/src/Components/PopUp/ComfirmPopup.js
+++ owl-mailbox-front/src/Components/PopUp/ComfirmPopup.js
@@ -3,79 +3,63 @@ import styled from "styled-components";
 
 /* 팝업창 박스 */
 const Container = styled.div`
-    position: absolute;
-    left: 40%;
-    top: 0%;
+  position: absolute;
+  left: 40%;
+  top: 20%;
 
-    width: 270px;
-    height: 130px;
-    background-color: rgba(218, 221, 252, 0.8);
-    border-radius: 14px;
-    padding-top: 19px; 
+  width: 270px;
+  height: 110px;
+  background-color: rgba(218, 221, 252, 0.8);
+  border-radius: 14px;
+  padding-top: 10px; 
 
-    align-items: center;
-    text-align: center;
+  align-items: center;
+  text-align: center;
 `;
 
 /* 팝업 - 부엉이 우편함 타이틀 */
 const Title = styled.div`
-    font-weight: bolder;
-    font-size: 17px;
+  font-weight: bolder;
+  font-size: 17px;
 `;
 
-/* 팝업 - 부엉이 우편함 gps 설명 */
+/* 팝업 - 닉네임 사용 가능 여부 설명란 */
 const Explain = styled.div`
-    font-weight: normal;
-    margin-top: 7px;
-    font-size: 13px;
+  font-weight: normal;
+  margin-top: 7px;
+  font-size: 13px;
 `;
 
 /* 허용 버튼 상단 선 */
 const StyledLine = styled.hr`
-    margin-top: 20px;
-    margin-bottom: 0px;
-    border-top: 1px solid rgba(60, 60, 67, 0.29);
+  margin-top: 20px;
+  margin-bottom: 5px;
+  border-top: 1px solid rgba(60, 60, 67, 0.29);
 `;
 
-/* 버튼 플렉스 박스 */
-const BtnBox = styled.section`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-`;
-
+/* 확인 버튼 */
 const Button = styled.button`
-    flex-grow: 1;
-    flex-basis: 90px;
-    font-size: 17px;
-    font-weight: bold;
-    color: #007AFF;
-    background-color: transparent !important;
-    outline: none;
-    border: none;
+  font-size: 17px;
+  font-weight: bold;
+  color: #007AFF;
+  background-color: transparent !important;
+  outline: none;
+  border: none;
 `;
 
-const SeparateLine = styled.div`
-    height: 44px;
-    border-left: 1px solid rgba(60, 60, 67, 0.29);
-`;
-
-const ConfirmPopup = () => {
+const SelectPopup = () => {
     return (
       <>
         <Container>
-        <Title>부엉이 우편함</Title>
-        <Explain>
-            임시저장을 하지 않고 뒤로가기를 <br /> 선택할 경우 내용이 저장되지 않습니다.
-        </Explain>
-        <StyledLine />
-        <BtnBox>
-            <Button onClick="#">뒤로가기</Button>
-            <SeparateLine />
-            <Button onClick="#">계속 작성하기</Button>
-        </BtnBox>
-    </Container>
+            <Title>부엉이 우편함</Title>
+            <Explain>
+                사용 불가능한 닉네임입니다.
+            </Explain>
+            <StyledLine />
+            <Button onClick="">확인</Button>
+        </Container>
       </>
     );
   };
-  export default ConfirmPopup;
+  export default SelectPopup
+;
