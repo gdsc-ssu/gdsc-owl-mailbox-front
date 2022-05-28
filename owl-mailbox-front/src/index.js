212b996f3038294ad3817756bd2986aed22cab24
ywoo121 <ywoo121@naver.com>
2022-05-28 14:20:20 +0900

    deleteAccount_탈퇴하기

--- owl-mailbox-front/src/index.js
+++ owl-mailbox-front/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import NavBot from './Components/NavBot';
+import Create from './Components/Create';
+import DeleteAccount from './Components/MyPage/Setting/DeleteAccount';
 
-import MakingOwl from './Components/MyPage/Setting/MakingOwl';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBot />
-    <MakingOwl />
+    <NavBot/>
+    <DeleteAccount />
   </React.StrictMode>
 );
 
 //TODO : Router 사용해서 루트짜기
-//TODO : NavBot..  -> 상단 이름값 받아오기
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
