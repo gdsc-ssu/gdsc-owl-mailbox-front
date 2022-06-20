03279fe8338c56cd589b89ebd83c045a9a8b9a51
seonyoungan <asy093@naver.com>
2022-06-21 03:17:49 +0900

    feat: 카카오 로그인 API 연결

---
+++ owl-mailbox-front/src/Routes/Login.js
@@ -0,0 +1,14 @@
+import Login from "../Components/Login/Login";
+import NavBot from "../Components/NavBot";
+
+const LoginPresenter = () => {
+    return (
+        <>
+        <NavBot name="로그인" />
+        <Login />
+        </>
+
+    )
+}
+
+export default LoginPresenter;
