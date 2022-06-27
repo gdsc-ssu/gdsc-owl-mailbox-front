7d8f60572f8c0e7f88b0f414dd19e290a4ef836c
seonyoungan <asy093@naver.com>
2022-06-28 02:37:40 +0900

    chore: 카카오 로그인 api 연결(수정중)

--- owl-mailbox-front/src/Components/Login/LoginCallback.js
+++ owl-mailbox-front/src/Components/Login/LoginCallback.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { REST_API_KEY, REDIRECT_URI } from "./LoginData";
 import { useLocation, useNavigate  } from "react-router";
 import QueryString from "qs";
 import NicknameRandom from "./NicknameRandom";
 
 
-function LoginCallback(){
+const LoginCallback = () =>{
     const location = useLocation();
     const navigate = useNavigate();
     const KAKAO_CODE = location.search.split('=')[1];
@@ -13,7 +13,7 @@ function LoginCallback(){
     /* 카카오에서 회원 토큰 발급 */
     const getKakaoToken = async() => {
         try{
-            await fetch(`https:kauth.kakao.com/oauth/token`, {
+            fetch(`https:kauth.kakao.com/oauth/token`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded', },
                 body: QueryString.stringify({
@@ -31,77 +31,19 @@ function LoginCallback(){
                     
                 } else if(data.refresh_token){
                     console.log("Success:", data);
-                    
+                    localStorage.setItem('refreshToken', data.refresh_token);
                 }
             });
             navigate("/");
         } catch(err){
-            console.log("error");
+            console.log(err);
         }
     }
 
-    /* 서버로 회원 토큰 전송 (회원가입) */
-    const sendSignupToken = async() => {
-        try{
-            await fetch('http://ec2-52-79-45-176.ap-northeast-2.compute.amazonaws.com:3001/signup/v1/kakao', {
-                method: 'POST',
-                headers: { 'Content-Type' : 'application/json', },
-                body: JSON.parse({
-                    'accessToken': getKakaoToken.data.access_token,
-                    'nickname': NicknameRandom(),
-                })
-            })
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .then(navigate('/'));
-        } catch(err){
-            console.log("error");
-        }
-    };
-
-    /* 서버로 회원 토큰 전송 (로그인) */
-    const sendLoginToken = async() => {
-        try{
-            await fetch('http://ec2-52-79-45-176.ap-northeast-2.compute.amazonaws.com:3001/auth/v1/kakao/login', {
-                method: 'POST',
-                headers: { 'Content-Type' : 'application/json', },
-                body: JSON.parse({
-                    'accessToken': getKakaoToken.data.access_token,
-                })
-            })
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .then(navigate('/'));
-        } catch(err){
-            console.log("error");
-        }
-    };
-
-    /* 서버로 리퀘스트 토큰 전송 (로그인) */
-    const sendrefreshToken = async() => {
-        try{
-            await fetch('http://ec2-52-79-45-176.ap-northeast-2.compute.amazonaws.com:3001/auth/refresh', {
-                method: 'POST',
-                headers: { 'Content-Type' : 'application/json', },
-                body: JSON.parse({
-                    'refreshToken': getKakaoToken.data.refresh_token,
-                })
-            })
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .then(navigate('/'));
-        } catch(err){
-            console.log("error");
-        }
-    };    
-
     useEffect(() => {
-        LoginCallback(getKakaoToken);
-        LoginCallback(sendSignupToken);
-        LoginCallback(sendLoginToken);
-    });
-
-    return <></>;
+        if(!location.search) return;
+        getKakaoToken();
+    }, []);
 }
 
 export default LoginCallback;
